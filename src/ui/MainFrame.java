4ce86caae309b9bc162351dd1b0a2a1504e5b4ca
rose <rose@hello>
2014-04-29 14:38:14 +0800

    hoiwhiowerb

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -5,10 +5,10 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame {
 	public MainFrame() {
 		// TODO Auto-generated constructor stub
-		setSize(1500, 900);
+		setSize(1000, 600);
 		setVisible(true);
 		setLayout(null);
-		setResizable(false);
+//		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }

29103766eaafdaee53bbcdf9df67f925cd1352f2
rose <rose@hello>
2014-04-29 14:46:46 +0800

    test1

--- src/control/Controler.java
+++ src/control/Controler.java
@@ -5,11 +5,10 @@ public class Controler {
 
 	public Controler(int width, int height) {
 		// TODO Auto-generated constructor stub
-		transferAbsoluteToXY = new TransferAbsoluteToXY( width,  height);
+		transferAbsoluteToXY = new TransferAbsoluteToXY(width, height);
 	}
 
 	public TransferAbsoluteToXY getTransferAbsoluteToXY() {
-		System.out.println("123");
 		return transferAbsoluteToXY;
 	}
 
